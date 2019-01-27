8a9f51ff7fb9c71aeb3551da525786b60acd348b
Maik Luchtmeyer <m.luchtmeyer@gmail.com>
2019-01-27 13:37:11 +0100

    Refactor
    -Update Django
    -Include Bootstrap with React-Bootstrap

--- backend/frontendapp/src/components/login.jsx
+++ backend/frontendapp/src/components/login.jsx
@@ -1,62 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Container from 'react-bootstrap/lib/Container';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Form from 'react-bootstrap/lib/Form';
+import Button from 'react-bootstrap/lib/Button';
 import { login } from '../actions/auth';
 
 class Login extends Component {
   static propTypes = {
-    login: PropTypes.func.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
+    _login: PropTypes.func.isRequired,
   }
 
   onSubmit = (e) => {
     e.preventDefault();
     const { username, password } = this.state;
-    const { login } = this.props;
-    login(username, password);
+    const { _login } = this.props;
+    _login(username, password);
   }
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
-        <div className="col-md-offset-5 col-md-3">
-          <h4>Blueprint Login</h4>
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control input-sm chat-input"
-              placeholder="username"
-              onChange={e => this.setState({ username: e.target.value })}
-            />
-            <input
-              type="password"
-              className="form-control input-sm chat-input"
-              placeholder="password"
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary btn-md">
-            Login
-          </button>
-        </div>
-      </form>
+      <Container fluid="True" style={{ height: '100vh' }}>
+        <Row className="h-100 align-items-center">
+          <Col md={{ span: 6, offset: 3 }} className="text-center">
+            <Form onSubmit={this.onSubmit}>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="name"
+                  placeholder="Enter name"
+                  onChange={e => this.setState({ username: e.target.value })}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  onChange={e => this.setState({ password: e.target.value })}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: (username, password) => {
-      return dispatch(login(username, password));
-    },
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  _login: (username, password) => {
+    dispatch(login(username, password));
+  },
+});
 
 export default connect(null, mapDispatchToProps)(Login);
--- backend/frontendapp/src/index.jsx
+++ backend/frontendapp/src/index.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import Login from './components/login';
+import Login from './components/Login.jsx';
 import Reducer from './reducer/reducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
